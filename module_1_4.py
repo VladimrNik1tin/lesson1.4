60d21cd5802c61b5e0edcdace27a5833757114cd
VladimrNik1tin <nikitin.vladimir99@mail.ru>
2024-09-22 12:24:01 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите любой текст: ')
+print(len(my_string.replace(" ", "")))  # print(len(my_string)) - для вывода всех символов, включая пробелы.
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
